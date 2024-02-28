403c895ad4fc898835faf8eafba271cfe9bc0b3a
Astrid <github@astrid.exposed>
2024-02-28 17:39:33 +0100

    minor minor changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // https://stackoverflow.com/a/6969486
 function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  return string.replace(/[.*+$?^${}()|[\]\\]/g, "\\$&");
 }
 
 export default function formatString(string, variables = {}, delimiters = {}) {
   if (typeof string !== "string") {
     throw new Error("Argument string is not of type string");
   }
+
   if (typeof variables !== "object") {
     throw new Error("Argument variables is not of type object");
   }
+
   for (const key in variables) {
     const regex = new RegExp(
       (delimiters.prefix || "{{") +
@@ -17,6 +19,7 @@ export default function formatString(string, variables = {}, delimiters = {}) {
         (delimiters.suffix || "}}"),
       "g"
     );
+
     string = string.replaceAll(regex, variables[key]);
   }
   return string;
